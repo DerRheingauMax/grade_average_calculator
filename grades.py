c86d21e2aadbad7771f08466596cc0c97b723de3
DerRheingauMax <max.rheingauschule@gmail.com>
2025-03-27 17:01:54 +0100

    Update grades.py

--- grades.py
+++ grades.py
@@ -1,5 +1,5 @@
 def translate_grade(grade_str:str):
-    grade_float:float
+    grade_float =0.0
     try:
         grade_int = int(grade_str)
         if grade_int not in [1,2,3,4,5,6]:
@@ -31,7 +31,7 @@ def translate_grade(grade_str:str):
     return grade_float
 
 def calculate_avrage(grades:list):
-    grades_sum:float
+    grades_sum= 0.0
     for i in grades:
         grades_sum += i
     return grades_sum/len(grades)
@@ -39,12 +39,20 @@ def calculate_avrage(grades:list):
 
 if __name__ == "__main__":
     grades = []
+    grades_str = ""
     while True:
         grade = input("[grade]/result: ")
         if grade == "":
             break
-        elif translate_grade(grade) != False:
-            grades.append(translate_grade(grade))
         elif grade in ["r","result"]:
+            print(f'The avrage of the grades {grades_str}:')
             print(calculate_avrage(grades))
             break
+        elif translate_grade(grade) != False:
+            grades.append(translate_grade(grade))
+            if len(grades_str) == 0:
+            	grades_str += grade
+            else:
+            	grades_str += f', {grade}'
+        elif translate_grade(grade) == False:
+        	print("Error: wrong Input: Input was ignored")
